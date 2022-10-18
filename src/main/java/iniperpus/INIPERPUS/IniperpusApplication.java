d94eb03998882edcedfaf0c3d89365f26ada7a89
muhammadbarik26 <mhdbarik26@gmail.com>
2022-10-18 17:44:48 +0700

    add plugins maven

---
+++ src/main/java/iniperpus/INIPERPUS/IniperpusApplication.java
@@ -0,0 +1,13 @@
+package iniperpus.INIPERPUS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IniperpusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IniperpusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iniperpus/INIPERPUS/IniperpusApplicationTests.java
@@ -0,0 +1,13 @@
+package iniperpus.INIPERPUS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IniperpusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
